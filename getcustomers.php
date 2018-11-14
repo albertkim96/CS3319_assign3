04e4653850bcc50371453c70075985613680c0ee
Albert <mkim584@uwo.ca>
2018-11-13 22:23:13 -0500

    Made a few changes to getcustomers

--- getcustomers.php
+++ getcustomers.php
@@ -1,12 +1,13 @@
 <?php
-    $query = "SELECT * ";
+    $query = "SELECT * fName, lName from customers";
     $result = mysqli_query($connection,$query);
     if (!$result) {
         die("databases query failed.");
     }
     while ($row = mysqli_fetch_assoc($result)) {
-        var_dump($row);
-        echo $row . "<br>";
+        echo $row["fName"];
+        echo " ";
+        echo $row["lName"];
     }
     mysqli_free_result($result);
 ?>
