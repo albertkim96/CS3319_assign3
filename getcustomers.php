d73af5153e4513ab8c59dd723d21fc12622e39ab
Albert <mkim584@uwo.ca>
2018-11-18 16:31:04 -0500

    First question

--- getcustomers.php
+++ getcustomers.php
@@ -1,16 +1,16 @@
 <?php
-    $query = "SELECT * from customers INNER JOIN agents ON customers.agentID=agents.agentID ORDER BY customers.lName";
+    $query = "SELECT * from customers INNER JOIN agents ON customers.agentID=agents.agentID ORDER BY lName";
     $result = mysqli_query($connection,$query);
     if (!$result) {
         die("databases query failed.");
     }
     while ($row = mysqli_fetch_assoc($result)) {
         echo '<div>';
-        echo '<b>Name:</b> ' . $row["customers.lName"] . ', ' . $row["customers.fName"] . '<br>';
+        echo '<b>Name:</b> ' . $row["lName"] . ', ' . $row["fName"] . '<br>';
         echo '<b>Customer ID:</b>' . $row["customerID"] . '<br>';
         echo '<b>Customer City:</b>' . $row["customers.city"] . '<br>';
         echo '<b>Phone Number:</b>' . $row["phoneNumber"] . '<br>';
-        echo '<b>Agent Name:</b> ' . $row["agents.lName"] . ', ' . $row["agents.fName"] . '<br>';
+        echo '<b>Agent Name:</b> ' . $row["lastName"] . ', ' . $row["firstName"] . '<br>';
         echo '<b>Agent City:</b>' . $row["agents.city"] . '<br>';
         echo '<b>Agent commission percent:</b>' . $row["agents.commissionPercent"] . '<br>';
         echo '<b>Agent city:</b>' . $row["agents.agentID"] . '<br>';
