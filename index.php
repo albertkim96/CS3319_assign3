678bc74f3e7de76053d2533f95de9687b65f5b4a
Albert <mkim584@uwo.ca>
2018-11-18 15:58:18 -0500

    Change

--- index.php
+++ index.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
     <title>Customers Databases</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="styling/styles.css">
     <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet"></head>
 <body>
-<script src="index.js"></script>
-<?php
+<!-- <script src="index.js"></script> -->
+<!--?php
     include "connecttodb.php";
-?>
+?> -->
 
-<h1>Customers Database</h1>
+<h1>Assignment 3 CS3319 Database</h1>
 Select your options:
 
-<h2> 1. Select any customers and display which products they bought </h2>
+<div id="navigation-bar"> 
+    <ul>
+        <li><a class="active" href="index.php">Go Home</a></li>
+        <li><a href="showcustomers.php">View Customer Purchases</a></li>
+        <li><a href="showproducts.php">Products</a></li>
+    </ul>
+</div>
+
+
+<!-- <h2> 1. Select any customers and display which products they bought </h2>
 <select> 
     <option value="1"> Select Here </option>
 </select>
-<?php  
+<!?php  
     include "getcustomers.php";
 ?>
 <hr>
@@ -26,12 +36,12 @@ Select your options:
 <h2> 2. Show the products table and decide to show it either ascending or descending by price </h2>
 <select> 
     <option value="1"> Select Here </option>
-    <?php  
+    <!?php  
         include "getcustomers.php";
     ?>
 </select>
 <hr>
-<hr>
+<hr> -->
 
 
 </body>
---
+++ showcustomers.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Showing customers Database</title>
+    <link rel="stylesheet" type="text/css" href="styling/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet"></head>
+<body>
+
