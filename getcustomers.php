5bec31a855ae5da6884026c7ab9ca3c4f51ed976
Albert <mkim584@uwo.ca>
2018-11-18 16:14:35 -0500

    First question

--- getcustomers.php
+++ getcustomers.php
@@ -1,12 +1,15 @@
 <?php
-    $query = "SELECT * from customers ORDER BY lName";
+    $query = "SELECT * from customers INNER JOIN agents ON customers.agentID=agentID ORDER BY lName";
     $result = mysqli_query($connection,$query);
     if (!$result) {
         die("databases query failed.");
     }
     while ($row = mysqli_fetch_assoc($result)) {
-        var_dump($row);
-        echo $row;
-    }
+        echo '<div>';
+		echo '<input type="radio" name="customers" value="' . $row["CustomerID"] . '" /><b>' . $row["CustomerID"] . '</b>:<br> <b>Name:</b> ' . $row["lName"] . ', ' . $row["fName"] . '<br>';
+		echo '<b>Agent:</b> ' . $row["fName"] . ' ' . $row["lName"] . '<br>';
+		echo '<b>city:</b> ' . $row["city"] . '<br>';
+		echo '</div>';
+	}
     mysqli_free_result($result);
 ?>
--- showcustomers.php
+++ showcustomers.php
@@ -7,3 +7,40 @@
     <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet"></head>
 <body>
 
+    <h1>Assignment 3 CS3319 Database</h1>
+    Select your options:
+
+    <div id="navigation-bar"> 
+        <ul>
+            <li><a class="active" href="index.php">Go Home</a></li>
+            <li><a href="showcustomers.php">View Customer Purchases</a></li>
+            <li><a href="showproducts.php">Products</a></li>
+        </ul>
+    </div>
+
+    <?php
+        include "connecttodb.php";
+    ?> 
+
+    <form action="#" method="post">
+
+        <div id="show">
+            <?php 
+                include 'getcustomers.php'
+            ?>
+        </div>
+
+        <!-- Div to organize all the buttons needed for ordering the products-->
+        <div id="organizebuttons">
+            <!-- Allows the user to select the way purchased products by a customer are organized -->
+            <input type="radio" name="order" value="ASC" checked="checked">Ascending<br>
+            <input type="radio" name="order" value="DESC">Descending<br>
+        </div>
+
+        <!-- Button for user to accept all values of search and search the products purchased by customer -->
+        <input type="submit" name="submit" value="Show Purchased Products" id="submit"><br><hr><br>
+
+    </form>
+</body>
+</html>
+
