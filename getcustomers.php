46c630208c580cd95dd6418aebbe8c91d5c2c1a2
Albert <mkim584@uwo.ca>
2018-11-18 16:33:58 -0500

    First question

--- getcustomers.php
+++ getcustomers.php
@@ -11,9 +11,7 @@
         echo '<b>Customer City:</b>' . $row["customers.city"] . '<br>';
         echo '<b>Phone Number:</b>' . $row["phoneNumber"] . '<br>';
         echo '<b>Agent Name:</b> ' . $row["lastName"] . ', ' . $row["firstName"] . '<br>';
-        echo '<b>Agent City:</b>' . $row["agents.city"] . '<br>';
-        echo '<b>Agent commission percent:</b>' . $row["agents.commissionPercent"] . '<br>';
-        echo '<b>Agent city:</b>' . $row["agents.agentID"] . '<br>';
+        echo '<b>Agent City:</b>' . $row["agentCity"] . '<br>';
 		echo '</div>';
 	}
     mysqli_free_result($result);
