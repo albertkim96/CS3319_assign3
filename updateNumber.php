f99a847978ab363a329057cd0cbdee11315a1182
Albert <mkim584@uwo.ca>
2018-11-19 12:52:22 -0500

    Question 5

--- updateNumber.php
+++ updateNumber.php
@@ -1,7 +1,5 @@
 <?php 
-    include "connecttodb.php";
-    $phone_query = 'UPDATE customers SET phoneNumber="' . $_POST["newCustomerNumber"] . '" WHERE customerID=' . $_POST["customerID"];
-    
+    $phone_query = 'UPDATE customers SET phoneNumber="' . $_POST["newCustomerNumber"] . '" WHERE customerID=' . $_POST["customers"];
     if (mysqli_query($connection, $phone_query)) {
         echo 'Successfully updated';
     }
