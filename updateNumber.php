0553b76db706452c26b27bed7a37bb14355220d9
Albert <mkim584@uwo.ca>
2018-11-19 12:56:34 -0500

    Trying to fix connection in question 5

--- updateNumber.php
+++ updateNumber.php
@@ -2,7 +2,7 @@
     include 'connecttodb.php';
     $phone_query = 'UPDATE customers SET phoneNumber="' . $_POST["newCustomerNumber"] . '" WHERE customerID=' . $_POST["customers"];
     $result = mysqli_query($connection, $phone_query);
-    if (mysqli_query($result)) {
+    if ($result) {
         echo 'Successfully updated';
     }
     else {
