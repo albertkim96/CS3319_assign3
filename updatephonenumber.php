db5ed285b43645d278060a789d9c1ebc1b5bcee8
Albert <mkim584@uwo.ca>
2018-11-30 09:38:17 -0500

    Trying to show customers in updating phone number

--- updatephonenumber.php
+++ updatephonenumber.php
@@ -10,30 +10,38 @@
     <h1>Assignment 3 CS3319 Database</h1>
     Select your options:
 
-    <div id="navigation-bar"> 
+    <div id="navigation-bar">
         <ul>
-            <li><a class="active" href="index.php">Go Home</a></li>
+            <li><a href="index.php">Go Home</a></li>
             <li><a href="showcustomers.php">View Customer Purchases</a></li>
             <li><a href="showproducts.php">View Products</a></li>
             <li><a href="newpurchase.php">Insert a new Purchase</a></li>
             <li><a href="newcustomer.php">Adding a customer</a></li>
-            <li><a href="updatephonenumber.php">Update a customer's phone number</a></li>
-            
+            <li><a class="active" href="updatephonenumber.php">Update a customer's phone number</a></li>
+            <li><a href="deletecustomer.php">Delete a customer</a></li>
+            <li><a href="listcustomers.php">List all customers who bought more</a></li>
+            <li><a href="productsnotpurchased.php">List products not purchased</a></li>
+            <li><a href="totalnumber.php">List total number of purchases for a product</a></li>
         </ul>
     </div>
 
     <!-- Connect to database -->
-    <?php 
+    <?php
         include "connecttodb.php";
     ?>
 
-    <!-- Connect to customers database --> 
+    <!-- Connect to customers database -->
     <div id="container">
-        <form action="updateNumber.php" method="post">
-            <?php 
-                include 'getcustomerinfo.php';
+        <form action="#" method="post">
+            <?php
+                $query = 'SELECT * FROM customers';
+                $result = mysqli_query($connection, $query);
+                # Check if query worked
+                if (!$result) {
+                  die("Query failed");
+                }
 
-                echo '<select name="customers">';
+                echo '<select name="customerlist">';
 
                 while ($row = mysqli_fetch_assoc($result)) {
                     echo '<option value =' . $row["customerID"] . ' ' . $row["lName"] . ', ' . $row["fName"] . ' - ' . $row["phoneNumber"] . '</option>';
@@ -44,6 +52,8 @@
 
         <input type="text" name="newCustomerNumber" placeholder="Input new phone number"><br>
         <input type="submit" value="Update customer's phone number">
+
+
     </div>
 
 </body>
