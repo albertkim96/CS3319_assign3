2d61d03147752c01d3f4c850e26a6b712f433e3d
Albert <mkim584@uwo.ca>
2018-12-03 10:56:26 -0500

    Removed styling for customers

--- showcustomers.php
+++ showcustomers.php
@@ -9,7 +9,6 @@ File: showcustomers.php -->
     <meta charset="utf-8">
     <title>Showing customers Database</title>
     <link rel="stylesheet" type="text/css" href="styling/styles.css">
-    <link rel="stylesheet" type="text/css" href="styling/showCustomer_styles.css">
     <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet"></head>
 <body>
 
